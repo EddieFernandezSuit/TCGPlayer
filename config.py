133a4897dbab4aa1e63b8903144e1d32f3775b22
EddieFernandezSuit <83356892+EddieFernandezSuit@users.noreply.github.com>
2024-03-18 19:18:11 -0700

    updates

--- config.py
+++ config.py
@@ -2,6 +2,3 @@ import pandas as pd
 
 DOWNLOADS_DIRECTORY=pd.read_csv('config.csv')['DOWNLOADS_DIRECTORY'][0]
 PROJECT_DIRECTORY=pd.read_csv('config.csv')['PROJECT_DIRECTORY'][0]
-# DOWNLOADS_DIRECTORY="C:\\Users\\ferna\\Downloads"
-# PROJECT_DIRECTORY="C:\\Users\\ferna\\Downloads\\Github\\TCGPLAYER\\"
-
