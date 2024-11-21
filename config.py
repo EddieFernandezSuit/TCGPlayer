ffdd0b8ab4b787e117948ffbc03c66ccbd4c8071
EddieFernandezSuit <83356892+EddieFernandezSuit@users.noreply.github.com>
2024-11-21 09:54:47 -0800

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-DOWNLOADS_DIRECTORY = "C:\\Users\\ferna\\Downloads"
-PROJECT_DIRECTORY = "C:\\Users\\ferna\\Downloads\\Github\\TCGPLAYER\\"
-PRINT_ENVELOPES_DIRECTORY = PROJECT_DIRECTORY + '.secrets\\'
-PASSWORD = 'Cjs!eh37hduH7'
