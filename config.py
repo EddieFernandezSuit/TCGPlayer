fb97d82600d8cb811c83673931ea4de3956c42d4
EddieFernandezSuit <83356892+EddieFernandezSuit@users.noreply.github.com>
2025-07-12 10:54:42 -0700

    normal changes

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-PROJECT_DIRECTORY=r"C:\Users\ferna\Downloads\TCGPlayer"
-DOWNLOADS_DIRECTORY=r"C:\Users\ferna\Downloads"
+PROJECT_DIRECTORY="C:\\Users\\ferna\\Downloads\\TCGPlayer\\"
+DOWNLOADS_DIRECTORY="C:\\Users\\ferna\\Downloads\\"
